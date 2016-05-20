5171ab50b21d7a77e27062e881ef0f0458b38639
tkmagesh <tkmagesh77@gmail.com>
2016-05-20 11:39:50 +0530

    Data file for server communication

--- Day-03/02-Async/program.js
+++ Day-03/02-Async/program.js
@@ -12,22 +12,37 @@ var program = (function(){
 		console.log(`[Consumer] result = ${result}`);
 	}
 
-	function addAsync(x,y){
+	function addAsyncCallback(x,y, onResult){
 		console.log(`     [Service] processing ${x} and ${y}`);
 		setTimeout(function(){
 			var result = x + y;
 			console.log(`     [Service] returning result`);
-			return result;
+			if (typeof onResult === 'function')
+				onResult(result);
 		},3000);
 	}
 
-	function addAsyncClient(x,y){
-		console.log('[Consumer] triggering addAsync');
-		var result = addAsync(x,y);
-		console.log(`[Consumer] result = ${result}`);
+	function addAsyncCallbackClient(x,y){
+		console.log('[Consumer] triggering addAsyncCallback');
+		addAsyncCallback(x,y, function(result){
+			console.log(`[Consumer] result = ${result}`);
+		});
+	}
+
+	function addAsyncPromise(x,y){
+		console.log(`     [Service] processing ${x} and ${y}`);
+		var promise = new Promise(function(resolveFn, rejectFn){
+			setTimeout(function(){
+				var result = x + y;
+				console.log(`     [Service] returning result`);
+				resolveFn(result);
+			},3000);
+		});
+		return promise;
 	}
 	return {
 		addSyncClient : addSyncClient,
-		addAsyncClient : addAsyncClient
+		addAsyncCallbackClient : addAsyncCallbackClient,
+		addAsyncPromise : addAsyncPromise
 	}
 })();
